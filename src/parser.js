076d414a43ab41a9f0c42e2a94370c3ab1d8782e
Rahul Kadyan <hi@znck.me>
2017-10-03 08:16:53 +0530

    feat: SFC to descriptors parser

---
+++ src/parser.js
@@ -0,0 +1,64 @@
+const compiler = require('vue-template-compiler')
+const LRU = require('lru-cache')
+const hash = require('hash-sum')
+const { SourceMapGenerator } = require('source-map')
+
+const cache = LRU(100)
+const splitRE = /\r?\n/g
+const emptyRE = /^(?:\/\/)?\s*$/
+
+module.exports = function (content, filename, needMap) {
+  const cacheKey = hash(filename + content)
+  const filenameWithHash = filename + '?' + cacheKey // source-map cache busting for hot-reloadded modules
+
+  if (cache.has(cacheKey)) return cache.get(cacheKey)
+
+  const output = compiler.parseComponent(content, { pad: 'line' })
+  if (needMap) {
+    if (output.script && !output.script.src) {
+      output.script.map = generateSourceMap(
+        filenameWithHash,
+        content,
+        output.script.content
+      )
+    }
+    if (output.styles) {
+      output.styles.forEach(style => {
+        if (!style.src) {
+          style.map = generateSourceMap(
+            filenameWithHash,
+            content,
+            style.content
+          )
+        }
+      })
+    }
+  }
+  cache.set(cacheKey, output)
+
+  return output
+}
+
+function generateSourceMap (filename, source, generated) {
+  const map = new SourceMapGenerator()
+
+  map.setSourceContent(filename, source)
+
+  generated.split(splitRE).forEach((line, index) => {
+    if (!emptyRE.test(line)) {
+      map.addMapping({
+        source: filename,
+        original: {
+          line: index + 1,
+          column: 0
+        },
+        generated: {
+          line: index + 1,
+          column: 0
+        }
+      })
+    }
+  })
+
+  return map.toJSON()
+}
