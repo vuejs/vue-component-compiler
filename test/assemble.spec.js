f8a36c7c6656f2f8ca3a42449c11e7846099c544
Rahul Kadyan <hi@znck.me>
2018-01-13 18:15:48 +0000

    feat: support wildcard tag in transformToRequire (#41)
    fixes #40

--- test/assemble.spec.js
+++ test/assemble.spec.js
@@ -3,6 +3,14 @@ import { mount } from '@vue/test-utils'
 // Fixtures
 import Transform from './fixtures/transform.vue'
 
+test('transfom to require', () => {
+  const wrapper = mount(Transform)
+  const data = /\s*data:([a-z]+\/[a-z]+(;[a-z\-]+\=[a-z\-]+)?)?(;base64)?,[a-z0-9\!\$\&\'\,\(\)\*\+\,\;\=\-\.\_\~\:\@\/\?\%\s]*\s*/i
+
+  expect(wrapper.find('img').attributes().src).toEqual(expect.stringMatching(data))
+  expect(wrapper.element.children[2].children[0].getAttribute('xlink:href')).toEqual(expect.stringMatching(data))
+})
+
 test('transform srcset', () => {
   const wrapper = mount(Transform)
   const images = wrapper.findAll('img')
