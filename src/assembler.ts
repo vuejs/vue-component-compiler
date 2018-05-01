28881537c950117eb88a6903b86e0081698a4e21
Rahul Kadyan <hi@znck.me>
2018-05-01 11:38:10 +0000

    docs: Add API docs (#60)
    * refactor: assemble method
    
    * docs: add API docs

--- src/assembler.ts
+++ src/assembler.ts
@@ -4,7 +4,14 @@ export interface AssembleSource {
   filename: string
   script?: { source: string; map?: any }
   template?: { source: string; functional?: boolean }
-  styles: Array<{ source: string; scoped?: boolean; map?: any }>
+  styles: Array<{
+    source: string
+    scoped?: boolean
+    media?: string
+    map?: any
+    moduleName?: string
+    module?: any
+  }>
   scopeId: string
 }
 
@@ -13,7 +20,7 @@ export interface AssembleResults {
   map?: any
 }
 
-export function assemble (
+export function assemble(
   compiler: SFCCompiler,
   filename: string,
   result: DescriptorCompileResult
@@ -42,7 +49,7 @@ export function assemble (
   })
 }
 
-export function assembleFromSource (
+export function assembleFromSource(
   compiler: SFCCompiler,
   { filename, script, template, styles, scopeId }: AssembleSource
 ): AssembleResults {
@@ -52,34 +59,140 @@ export function assembleFromSource (
   const hasScopedStyle = styles.some(style => style.scoped === true)
   const hasStyle = styles.length > 0
   const e = (any: any): string => JSON.stringify(any)
-  const code = `
-/* template */
-${template.source
-    .replace('var render =', 'var __vue_render__ =')
-    .replace('var staticRenderFns =', 'var __vue_staticRenderFns__ =')
-    .replace('render._withStripped =', '__vue_render__._withStripped =')}
-const __vue_template__ = ${
-  template.source.includes('var render =')
-    ? '{ render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ }'
-    : '{ render() {} }'
-}
-/* style */
-const __vue_inject_styles__ = ${e(hasStyle)} ? function (context) {
-${styles.map(
-    (style, index) => `context(${e(scopeId + ':' + index)}, ${e(style)})\n`
-  )}
-} : undefined
-/* script */
-${script.source.replace(/export default/, 'const __vue_script__ =')}
-/* scoped */
-const __vue_scope_id__ = ${e(hasScopedStyle)} ? ${e(scopeId)} : undefined
-/* functional template */
-const __vue_is_functional_template__ = ${e(template.functional)}
-/* component normalizer */
-function __vue_normalize__(template, inject, script, scope, functional) {
+
+  const o = e
+
+  // language=JavaScript
+  const createInjector = `function __vue_create_injector__() {
+  const head = document.head || document.getElementsByTagName('head')[0]
+  const styles = {}
+  const isOldIE =
+    typeof navigator !== 'undefined' &&
+    /msie [6-9]\\\\b/.test(navigator.userAgent.toLowerCase())
+
+  return function addStyle(id, css) {
+    if (document.querySelector('style[data-vue-ssr-id~="' + id + '"]')) return // SSR styles are present.
+
+    const group = isOldIE ? css.media || 'default' : id
+    const style = styles[group] || (styles[group] = { ids: [], parts: [], element: undefined })
+
+    if (!style.ids.includes(id)) {
+      let code = css.source
+      let index = style.ids.length
+
+      style.ids.push(id)
+
+      if (${e(compiler.template.isProduction)} && css.map) {
+        // https://developer.chrome.com/devtools/docs/javascript-debugging
+        // this makes source maps inside style tags work properly in Chrome
+        code += '\\n/*# sourceURL=' + css.map.sources[0] + ' */'
+        // http://stackoverflow.com/a/26603875
+        code +=
+          '\\n/*# sourceMappingURL=data:application/json;base64,' +
+          btoa(unescape(encodeURIComponent(JSON.stringify(css.map)))) +
+          ' */'
+      }
+
+      if (isOldIE) {
+        style.element = style.element || document.querySelector('style[data-group=' + group + ']')
+      }
+
+      if (!style.element) {
+        const el = style.element = document.createElement('style')
+        el.type = 'text/css'
+
+        if (css.media) el.setAttribute('media', css.media)
+        if (isOldIE) {
+          el.setAttribute('data-group', group)
+          el.setAttribute('data-next-index', '0')
+        }
+
+        head.appendChild(el)
+      }
+
+      if (isOldIE) {
+        index = parseInt(style.element.getAttribute('data-next-index'))
+        style.element.setAttribute('data-next-index', index + 1)
+      }
+
+      if (style.element.styleSheet) {
+        style.parts.push(code)
+        style.element.styleSheet.cssText = style.parts
+          .filter(Boolean)
+          .join('\\n')
+      } else {
+        const textNode = document.createTextNode(code)
+        const nodes = style.element.childNodes
+        if (nodes[index]) style.element.removeChild(nodes[index])
+        if (nodes.length) style.element.insertBefore(textNode, nodes[index])
+        else style.element.appendChild(textNode)
+      }
+    }
+  }
+}`
+
+  // language=JavaScript
+  const createInjectorSSR = `function __vue_create_injector_ssr__(context) {
+  if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+    context = __VUE_SSR_CONTEXT__
+  }
+
+  if (!context) return function () {}
+
+  if (!context.hasOwnProperty('styles')) {
+    Object.defineProperty(context, 'styles', {
+      enumerable: true,
+      get: () => context._styles
+    })
+    context._renderStyles = renderStyles
+  }
+
+  function renderStyles(styles) {
+    let css = ''
+    for (const {ids, media, parts} of styles) {
+      css +=
+        '<style data-vue-ssr-id="' + ids.join(' ') + '"' + (media ? ' media="' + media + '"' : '') + '>'
+        + parts.join('\\n') +
+        '</style>'
+    }
+
+    return css
+  }
+
+  return function addStyle(id, css) {
+    const group = ${e(
+      compiler.template.isProduction
+    )} ? css.media || 'default' : id
+    const style = context._styles[group] || (context._styles[group] = { ids: [], parts: [] })
+
+    if (!style.ids.includes(id)) {
+      style.media = css.media
+      style.ids.push(id)
+      let code = css.source
+      if (${e(!compiler.template.isProduction)} && css.map) {
+        // https://developer.chrome.com/devtools/docs/javascript-debugging
+        // this makes source maps inside style tags work properly in Chrome
+        code += '\\n/*# sourceURL=' + css.map.sources[0] + ' */'
+        // http://stackoverflow.com/a/26603875
+        code +=
+          '\\n/*# sourceMappingURL=data:application/json;base64,' +
+          btoa(unescape(encodeURIComponent(JSON.stringify(css.map)))) +
+          ' */'
+      }
+      style.parts.push(code)
+    }
+  }
+}`
+
+  // language=JavaScript
+  const normalizeComponent = `function __vue_normalize__(
+  template, style, script,
+  scope, functional, moduleIdentifier,
+  createInjector, createInjectorSSR
+) {
   const component = script || {}
 
-  if (!${e(compiler.template.isProduction)}) {
+  if (${e(!compiler.template.isProduction)}) {
     component.__file = ${e(filename)}
   }
 
@@ -94,166 +207,112 @@ function __vue_normalize__(template, inject, script, scope, functional) {
   component._scopeId = scope
 
   if (${e(hasStyle)}) {
-    const call_inject = function(context) {
-      inject((id, style) => {
-        if (style.moduleName) {
-          if (${e(
-    !compiler.template.isProduction
-  )} && Object.prototype.hasOwnProperty.call(this, style.moduleName)) {
-            console.log('CSS module name (' + style.moduleName + ') is reserved.')
-          } else Object.defineProperty(this, style.moduleName, {
-            value: style.module
-          })
-        }
-        let css = style.source
-        if (style.map) {
-          // https://developer.chrome.com/devtools/docs/javascript-debugging
-          // this makes source maps inside style tags work properly in Chrome
-          css += '\\n/*# sourceURL=' + style.map.sources[0] + ' */'
-          // http://stackoverflow.com/a/26603875
-          css +=
-            '\\n/*# sourceMappingURL=data:application/json;base64,' +
-            window.btoa(unescape(encodeURIComponent(JSON.stringify(sourceMap)))) +
-            ' */'
-        }
-        if (${e(compiler.template.optimizeSSR)}) {
-          // 2.3 injection
-          context =
-            context || // cached call
-            (this.$vnode && this.$vnode.ssrContext) || // stateful
-            (this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext) // functional
-          // 2.2 with runInNewContext: true
-          if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
-            context = __VUE_SSR_CONTEXT__
-          }
-          if (!context.hasOwnProperty('styles')) {
-            function renderStyles (styles) {
-              let css = ''
-              for (const key in styles) {
-                const it = styles[key]
-                css +=
-                  '<style data-vue-ssr-id="' + it.ids.join(' ') + '"' +
-                  (it.media ? ' media="' + it.media + '"' : '') +
-                  '>' +
-                  it.source +
-                  '</style>'
-              }
-              return css
-            }
-            Object.defineProperty(context, 'styles', {
-              enumerable: true,
-              get: () => renderStyles(context._styles)
-            })
-            // expose renderStyles for vue-server-renderer (vuejs/#6353)
-            context._renderStyles = renderStyles
-          }
-          const styles = context._styles || (context._styles = {})
-          const groupId = ${e(
-    compiler.template.isProduction
-  )} ? style.media || 'default' : id
-          const target = styles[groupId]
-          if (target) {
-            if (target.ids.indexOf(id) < 0) {
-              target.ids.push(id)
-              target.source += '\\n' + style.source
-            }
-          } else {
-            styles[groupId] = {
-              ids: [id],
-              style: style.source,
-              media: style.media
-            }
-          }
-        } else {
-          const head = document.head || document.getElementsByTagName('head')[0]
-          const isOldIE = typeof navigator !== 'undefined' && /msie [6-9]\\b/.test(navigator.userAgent.toLowerCase())
-          const styleElement = document.querySelector('style[data-vue-ssr-id~="' + id + '"]')
-          if (styleElement) {
-            // SSR styles are present.
-            return
-          }
-
-          if (isOldIE) {
-            const indexKey = '__vue_style_singleton_counter__'
-            const textKey = '__vue_style_text__'
-            window[indexKey] = window[indexKey] || 0
-            const styles = window[textKey] = window[textKey] || []
-            const index = ++window[indexKey]
-            let styleElement = document.querySelector('style[data-vue-style-singleton=' + (style.media || '""') + ']')
-            if (!styleElement) {
-              styleElement = document.createElement('style')
-              styleElement.setAttribute('data-vue-style-singleton', style.media || '')
-              styleElement.type = 'text/css'
-              head.appendChild(style)
-            }
-            if (styleElement.styleSheet) {
-              styles[index] = css
-              styleElement.styleSheet.cssText = style.filter(Boolean).join('\\n')
-            } else {
-              const cssNode = document.createTextNode(css)
-              const childNodes = styleElement.childNodes
-              if (childNodes[index]) styleElement.removeChild(childNodes[index])
-              if (childNodes.length) {
-                styleElement.insertBefore(cssNode, childNodes[index])
-              } else {
-                styleElement.appendChild(cssNode)
-              }
-            }
-          } else {
-            const styleElement = document.createElement('style')
-            styleElement.type = 'text/css'
-            style.media && styleElement.setAttribute('media', style.media)
-            head.appendChild(styleElement)
-            if (style.styleSheet) {
-              styleElement.styleSheet.cssText = css
-            } else {
-              styleElement.appendChild(document.createTextNode(css))
-            }
-          }
-        }
-      })
-      if (context && context._registeredComponents) {
-        context._registeredComponents.add(scope)
-      }
-    }
+    let hook
     if (${e(compiler.template.optimizeSSR)}) {
-      component._ssrRegister = call_inject
-    }
-    if (component.functional) {
-      const render = component.render
-      component.render = function renderWithInjection (h, ctx) {
-        call_inject.call(this, ctx)
-
-        return render(h, ctx)
+      // In SSR.
+      hook = function(context) {
+        // 2.3 injection
+        context =
+          context || // cached call
+          (this.$vnode && this.$vnode.ssrContext) || // stateful
+          (this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext) // functional
+        // 2.2 with runInNewContext: true
+        if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+          context = __VUE_SSR_CONTEXT__
+        }
+        // inject component styles
+        if (style) {
+          style.call(this, createInjectorSSR(context))
+        }
+        // register component module identifier for async chunk inference
+        if (context && context._registeredComponents) {
+          context._registeredComponents.add(moduleIdentifier)
+        }
       }
-    } else {
-      const name = 'beforeCreate'
-      component[name] = [call_inject].concat(component[name])
+      // used by ssr in case component is cached and beforeCreate
+      // never gets called
+      component._ssrRegister = hook
+    }
+    else if (style) {
+      hook = function(context) {
+        style.call(this, createInjector(context))
+      }
+    }
+
+    if (hook !== undefined) {
+      if (component.functional) {
+        // register for functional component in vue file
+        const originalRender = component.render
+        component.render = function renderWithStyleInjection(h, context) {
+          hook.call(context)
+          return originalRender(h, context)
+        }
+      } else {
+        // inject component registration as beforeCreate hook
+        const existing = component.beforeCreate
+        component.beforeCreate = existing ? [].concat(existing, hook) : [hook]
+      }
     }
   }
 
   return component
-}
+}`
+
+  // language=JavaScript
+  const code = `
+/* script */
+${script.source.replace(/export default/, 'const __vue_script__ =')}
+/* template */
+${template.source
+    .replace('var render =', 'var __vue_render__ =')
+    .replace('var staticRenderFns =', 'var __vue_staticRenderFns__ =')
+    .replace('render._withStripped =', '__vue_render__._withStripped =')}
+const __vue_template__ = typeof __vue_render__ !== 'undefined'
+  ? { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ }
+  : {}
+/* style */
+const __vue_inject_styles__ = ${hasStyle} ? function (inject) {
+  if (!inject) return
+  ${styles.map(
+    (style, index) =>
+      `inject("${scopeId + '_' + index}", ${o({
+        source: style.source,
+        media: style.media,
+        map: compiler.template.isProduction ? undefined : style.map
+      })})` +
+      '\n' +
+      (style.moduleName
+        ? `Object.defineProperty(this, "${style.moduleName}", { value: ${o(
+            style.module || {}
+          )} })` + '\n'
+        : '')
+  )}
+} : undefined
+/* scoped */
+const __vue_scope_id__ = ${e(hasScopedStyle)} ? "${scopeId}" : undefined
+/* module identifier */
+const __vue_module_identifier__ = ${e(
+    compiler.template.optimizeSSR
+  )} ? "${scopeId}" : undefined
+/* functional template */
+const __vue_is_functional_template__ = ${e(template.functional)}
+/* component normalizer */
+${normalizeComponent}
+/* style inject */
+${!compiler.template.optimizeSSR ? createInjector : ''}
+/* style inject SSR */
+${compiler.template.optimizeSSR ? createInjectorSSR : ''}
 
 export default __vue_normalize__(
   __vue_template__,
   __vue_inject_styles__,
-  __vue_script__,
+  typeof __vue_script__ === 'undefined' ? {} : __vue_script__,
   __vue_scope_id__,
-  __vue_is_functional_template__
-)
-`
-  // let map
-
-  // if (script.map) {
-  //   const startPointer = '/* script */\n'
-  //   const startIndex = code.indexOf(startPointer) + startPointer.length
-  //   const endIndex = code.indexOf('/* scoped */\n') - 1
-  //   const startLine = code.substr(0, startIndex).split('\n').length
-  //   const endLine = code.substr(0, endIndex).split('\n').length
-
-  //   // generate script source map.
-  // }
+  __vue_is_functional_template__,
+  __vue_module_identifier__,
+  typeof __vue_create_injector__ !== 'undefined' ? __vue_create_injector__ : function () {},
+  typeof __vue_create_injector_ssr__ !== 'undefined' ? __vue_create_injector_ssr__ : function () {}
+)`
 
   return { code }
 }
