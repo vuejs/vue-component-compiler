c60d8362d178892ff49d7252ebdd6e5296456853
Rahul Kadyan <hi@znck.me>
2018-08-28 07:25:05 +0530

    feat: Add compileToDescriptorAsync and compileStyleAsync methods

--- test/compile.spec.ts
+++ test/compile.spec.ts
@@ -1,4 +1,4 @@
-import {createDefaultCompiler} from "../src"
+import {createDefaultCompiler, DescriptorCompileResult} from "../src"
 
 it('should prepend data scss option to actual style', () => {
   const compiler = createDefaultCompiler({
@@ -15,4 +15,48 @@ it('should prepend data scss option to actual style', () => {
   );
 
   expect(result.code).toEqual(expect.stringContaining('color: red'))
+})
+
+const source = `
+<template>
+  <h1 id="test" class="title">Hello {{ name }}!</h1>
+</template>
+
+<script>
+export default {
+  data () {
+    return { name: 'John Doe' }
+  }
+}
+</script>
+
+<style>
+.title {
+  color: red;
+}
+</style>
+`
+
+it('should compile to descriptor', () => {
+  const compiler = createDefaultCompiler()
+  const result = compiler.compileToDescriptor('foo.vue', source)
+
+  expect(result).toMatchSnapshot()
+})
+
+it('should compile to descriptor (async)', async () => {
+  const compiler = createDefaultCompiler()
+  const expected = compiler.compileToDescriptor('foo.vue', source)
+  const result = await compiler.compileToDescriptorAsync('foo.vue', source)
+
+  function removeRawResult(result: DescriptorCompileResult): DescriptorCompileResult {
+    result.styles.map(style => {
+      delete style.rawResult
+    })
+
+    return result
+  }
+
+  expect(result).toMatchSnapshot()
+  expect(removeRawResult(result)).toEqual(removeRawResult(expected))
 })
