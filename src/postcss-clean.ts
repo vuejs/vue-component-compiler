baa5ea451e03234930a2ae9746ac82234468961c
Ilya Golovin <74474615+ILScc@users.noreply.github.com>
2022-05-14 19:12:51 +0300

    FIx: remove ts errors

--- src/postcss-clean.ts
+++ src/postcss-clean.ts
@@ -1,15 +1,16 @@
 import * as postcss from 'postcss'
+
 // ESM import of clean-css breaks test/runtime check this fix for reference:
 // https://github.com/vuejs/vue-component-compiler/pull/103#issuecomment-632676899
 const CleanCSS = require('clean-css')
 
-exports.default = (options): postcss.Plugin => {
+exports.default = (options: any): postcss.Plugin => {
   const clean = new CleanCSS(Object.assign({ compatibility: 'ie9' }, options))
   return {
     postcssPlugin: 'clean',
     Once(css, res) {
       const output = clean.minify(css.toString())
-      res.root = postcss.parse(output.styles)
+      res.root = () => postcss.parse(output.styles)
     },
   }
 }
